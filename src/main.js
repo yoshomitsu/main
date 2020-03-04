819159b11271cbb13301fe87ef2899e800b4e2c3
yoshomitsu <yoshomitsu@yahoo.com>
2020-03-04 10:24:54 -0500

    ‘vchat’

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Chat from './../../dist/index'
+
+import vmodal from 'vue-js-modal'
+
+Vue.use(vmodal, {
+  dialog: true
+})
+Vue.use(Chat, {})
+
+// eslint-disable-next-line
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
